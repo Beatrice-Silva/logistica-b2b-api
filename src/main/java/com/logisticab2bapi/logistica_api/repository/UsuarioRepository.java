bfc473dbbcd636f8dc3298a4ba8711f748110bf6
Beatrice <beatricesilva1113@gmail.com>
2026-07-30 16:52:46 -0300

    Update Usuario roles

--- src/main/java/com/logisticab2bapi/logistica_api/repository/UsuarioRepository.java
+++ src/main/java/com/logisticab2bapi/logistica_api/repository/UsuarioRepository.java
@@ -5,6 +5,7 @@
 package com.logisticab2bapi.logistica_api.repository;
 
 import com.logisticab2bapi.logistica_api.model.Usuario;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
@@ -18,7 +19,11 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     
     Optional<Usuario> findByEmail(String email);
     
+    List<Usuario> findByRole(enum PerfilRole = "ADMIN");
     
+    List<Usuario> findByRole(enum PerfilRole = "ENTREGADOR");
+    
+    List<Usuario> findByRole(enum PerfilRole = "OPERADOR");
     
     boolean existsByEmail(String email);
     }
